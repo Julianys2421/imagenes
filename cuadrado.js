9624a8306d244fb728feaad4cf1a632ecdd9f8da
Edison Rivera gutierrez <julianysp44@gmail.com>
2022-11-01 18:27:59 -0500

    imagenes

---
+++ cuadrado.js
@@ -0,0 +1,6 @@
+for(var i=1; i<=5; i++){
+    for(var j=1; j<=10; j++){
+        document.body.innerHTML+="<img src='r.jpg' class='circulo'>";
+    }
+    document.body.innerHTML+="<img src='r.jpg' class='circulo'>";
+}
